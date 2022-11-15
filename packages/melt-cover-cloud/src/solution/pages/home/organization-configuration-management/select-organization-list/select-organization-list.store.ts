553e94529307dfb372ea0e43f870e01c6d2a6a90
bazijun <1324022569@qq.com>
2022-11-15 11:27:34 +0800

    fix： 暂存

--- packages/melt-cover-cloud/src/solution/pages/home/organization-configuration-management/select-organization-list/select-organization-list.store.ts
+++ packages/melt-cover-cloud/src/solution/pages/home/organization-configuration-management/select-organization-list/select-organization-list.store.ts
@@ -1,7 +1,72 @@
 import { useStateStore } from '@fch/fch-tool';
+import { cloneDeep } from 'lodash';
+import { useEffect } from 'react';
+import { EquityPackageManageService } from '~/solution/model/services/equity-package-manage.service';
 import { SelectOrganizationListState } from './select-organization-list.interface';
 
 export function useOrganizationListStore() {
   const { state, setStateWrap, getState } = useStateStore(new SelectOrganizationListState());
-  return { state };
+  const equityPackageManageService: EquityPackageManageService = new EquityPackageManageService();
+  useEffect(() => {
+    getTableData();
+  }, []);
+
+  function getTableData() {
+    // form.validateFields().then(values => {
+    setStateWrap({ isLoading: true });
+    equityPackageManageService
+      .getPackageClassifylist({
+        // ...values,
+        // coursePackageClassifyId: values?.coursePackageClassifyId?.[0],
+        // state: STATE_ENUM.Enable
+      })
+      .subscribe(
+        (res: any) => {
+          const tableData = cloneDeep(
+            res.map((item: any) => {
+              item.children = [];
+              return item;
+            })
+          );
+          setStateWrap({ tableData, total: res?.length, isLoading: false });
+        },
+        () => {
+          setStateWrap({ isLoading: false });
+        }
+      );
+    // });
+  }
+
+  function onExpand(expanded: any, record: any) {
+    if (!expanded) {
+      return;
+    }
+    setStateWrap({ isLoading: true });
+    teachingService
+      .getPackageClassifylist({
+        parentId: record['id']
+        // state: STATE_ENUM.Enable
+      })
+      .subscribe(
+        (res: any) => {
+          const tableData = state.tableData.map((item: any) => {
+            if (item.id === record['id']) {
+              item.children = res.map((o: any) => ({ ...o, isLeaf: true }));
+            }
+            return item;
+          });
+          setStateWrap({ tableData, isLoading: false });
+        },
+        () => {
+          setStateWrap({ isLoading: false });
+        }
+      );
+  }
+
+  function changeTablePageIndex(pageIndex: number, pageSize: number) {
+    setStateWrap({ pageIndex, pageSize });
+    getTableData();
+  }
+
+  return { state, changeTablePageIndex, onExpand };
 }
